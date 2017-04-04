592e84c4d7b4fcb43efa3a7ec367f71db99af10e
Alex <afu@dynosense.com>
2017-04-03 20:32:30 -0700

    1

---
+++ mediaStreaming/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  mediaStreaming
+//
+//  Created by Alex on 4/3/17.
+//  Copyright © 2017 alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var statusProgress: UIProgressView!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var totalDurationLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+
+}
+
